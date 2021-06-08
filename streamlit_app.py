80b56cdf2140eefc91887a626b38dff4b945e5ff
Yuliia Nikolaenko <51966068+yuliianikolaenko@users.noreply.github.com>
2021-06-08 21:56:51 +0200

    g

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,67 +5,97 @@ import plotly.express as px
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
-
+from datetime import datetime
 
 #Titles and Mode selections
-st.sidebar.title("Menu")
-st.sidebar.radio("Navigate", ["Home", "Data", "Dashboard", "About"])
-selectbox = st.sidebar.selectbox('Type',('death','case'))
 st.sidebar.title("About")
 st.sidebar.info(
     """
-    This app in Open Source
+    This app is Open Source dashboard.
     """
 )
-st.sidebar.title("Comment")
-st.sidebar.info("Feel free to comment on work. The github link can be found "
-                "[here]()")
+st.sidebar.info("Feel free to collaborate and comment on the work. The github link can be found "
+                "[here](https://github.com/yuliianikolaenko/COVID_dashboard_proglib)")
 
+st.sidebar.title("Menu")
+st.sidebar.radio("Navigate", ["Intro", "Data", "Map"])
+selectbox = st.sidebar.selectbox('Type',('Total Cases','Total Deaths', 'Total Vaccinations'))
+select_type = st.sidebar.selectbox("Type", options=np.append([""], df['total_cases', 'total_deaths', 'total_vaccinations'])
 
-st.title("COVID DASHBOARD")
-st.write("""
-This web application will serve to analyze and visualize the spread of COVID-19""")
-st.markdown("## Symptoms")
-st.markdown(("* Fever or chills\n* Cough\n"
-             "* Shortness of breath or difficulty breathing\n"
-             "* Fatigue\n"
-             "* Muscle or body aches\n"
-             "* Headache\n"
-             "* Loss of taste or smell\n"
-             "* Sore throat\n"
-             "* Congestion or runny nose\n"
-             "* Nausea or vomiting\n"
-             "* Diarrhea"))
+if select_type != "":
+    df_type = df[df['total_cases', 'total_deaths', 'total_vaccinations']]
+    show_timerange = True
+#st.image('covid.jpeg')
 
-
-st.image('covid.jpeg')
+st.title("COVID 19 IN THE WORLD DASHBOARD")
+st.write("""This dashboard will present the spread of COVID-19 in the world""")
+st.markdown("### Data on COVID-19 (coronavirus) by Our World in Data could be found [here](https://github.com/owid/covid-19-data/tree/master/public/data)")
 
 #Load Data
-df_case = pd.read_csv('Clean_Confirmed_Case.csv')
-df_case = df_case.set_index(['Date'])
-df_Death = pd.read_csv('Clean_Death.csv')
-df_Death = df_Death.set_index(['Date'])
-df_Recovered = pd.read_csv('Clean_Recovered.csv')
-df_Recovered = df_Recovered.set_index(['Date'])
+df = pd.read_csv('owid-covid-data.csv')
+df = df[df.location != 'World']
+df["date"] = pd.to_datetime(df["date"])
+# Calculate the timerange for the slider
+min_ts = min(df["date"]).to_pydatetime()
+max_ts = max(df["date"]).to_pydatetime()
 
+##### SIDEBAR
+#slider to chose date
+show_timerange = st.sidebar.checkbox("Show date range")
+day_date = pd.to_datetime(st.sidebar.slider("Date to chose", min_value=min_ts, max_value=max_ts, value=max_ts))
+select_country = st.sidebar.selectbox("Countries", options=np.append([""], df['location'].sort_values().unique()), index=0)
 
-#Chart the Datas
-selectbox = st.sidebar.selectbox('Type',('death','case'))
+if select_country != "":
+    df_country = df[df['location'] == select_country]
+    show_timerange = True
 
+if show_timerange:
+    min_selection, max_selection = st.sidebar.slider("Timeline", min_value=min_ts, max_value=max_ts,
+                                                     value=[min_ts, max_ts])
 
-if selectbox == 'death':
-    st.title("Cumulative number of deaths")
-    case = st.multiselect('choose country',df_case.columns)
+    # Filter data for timeframe
+    st.write(f"Filtering between {min_selection.date()} & {max_selection.date()}")
 
-    print(case)
+    df = df[(df["date"] >= min_selection) & (df["date"] <= max_selection)]
+    st.write(f"Country: {len(df)}")
 
-    fig = px.line(df_case, x=df_case.index, y=case)
-    st.write(fig)
 else:
-    st.title("Cumulative number of cases")
-    death = st.multiselect('choose country', df_Death.columns)
+    # Get last day data
+    #     day_date = pd.to_datetime(year + month + day, format='%Y%m%d')
+    st.write(f"Data for {day_date.date()}")
+    df = df[(df["date"] == day_date)]
+    st.write(f"Data Points: {len(df)}")
 
-    print(death)
 
-    fig = px.line(df_Death, x=df_Death.index, y=death)
-    st.write(fig)
+fig1 = px.choropleth(df, locations="iso_code",
+                    color="total_cases",
+                    hover_name="location",
+                    animation_frame="date",
+                    title = "Total COVID 19 cases in the world",
+                    color_continuous_scale=px.colors.sequential.Redor)
+
+fig1["layout"].pop("updatemenus")
+st.plotly_chart(fig1)
+
+
+fig2 = px.choropleth(df, locations="iso_code",
+                    color="total_deaths",
+                    hover_name="location",
+                    animation_frame="date",
+                    title = "Total deaths from COVID 19 in the world",
+                    color_continuous_scale=px.colors.sequential.Greys)
+
+fig2["layout"].pop("updatemenus")
+st.plotly_chart(fig2)
+
+
+fig3 = px.choropleth(df, locations="iso_code",
+                    color="total_vaccinations",
+                    hover_name="location",
+                    animation_frame="date",
+                    title = "Total vaccinated people in the world",
+                    color_continuous_scale=px.colors.sequential.Greens)
+
+fig3["layout"].pop("updatemenus")
+st.plotly_chart(fig3)
+
