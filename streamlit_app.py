0ba90c9eb2461d571a8f0e069aa2a8c5d270b28c
Yuliia Nikolaenko <51966068+yuliianikolaenko@users.noreply.github.com>
2021-06-08 20:28:46 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 
 
 #Titles and Mode selections
